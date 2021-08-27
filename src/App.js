ec90e6eb8a40f6cc32f1a25fca4629d023a564a0
Jorge Andrés Mena <jormencar@yahoo.com>
2021-08-27 09:27:34 +0100

    add color input

--- src/App.js
+++ src/App.js
@@ -19,16 +19,23 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
 
   const timeUpdateHandler = (event) => {
     const current = event.target.currentTime;
     const duration = event.target.duration;
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
     setSongInfo({
       ...songInfo,
       currentTime: current,
       duration,
+      animationPercentage: animation,
     });
   };
 
